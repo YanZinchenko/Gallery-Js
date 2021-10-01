c3855e61d938465310fac2bfde13e93ce2a3eb7a
YanZinchenko <Zinchenko.yan@gmail.com>
2021-10-01 22:07:17 +0300

    fix error oon line 6

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const slides = document.querySelectorAll('.slide')
 for (const slide of slides) {
     slide.addEventListener('click', () => {
         clearActiveClasses()
-        slide.classList.add('actice')
+        slide.classList.add('active')
     })
 }
 
